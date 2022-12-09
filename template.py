83d5280c6354e2bd6d2861d3f1c1c2137e6eb031
Noah Mollerstuen <35637345+NoahMollerstuen@users.noreply.github.com>
2022-12-09 11:56:43 -0500

    Update template

--- template.py
+++ template.py
@@ -2,10 +2,10 @@ from util import *
 from util import Grid as g
 import itertools as it
 import re
-import numpy as np
 
 
-h = Helper(test_mode=False)
+h = Helper(test_mode=True, test_input="""
+""")
 
 inp = h.get_input_list()
 
